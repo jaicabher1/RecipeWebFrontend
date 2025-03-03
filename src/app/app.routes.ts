915c09d03f0510243e7e69d2e4c05cf9173addea
jaicabher1 <jaicabher1@alum.us.es>
2025-03-03 20:56:25 +0100

    feat: home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 //Components
 import { userGuard } from './services/user/user.guard';
+import { HomeComponent } from './components/home/home.component';
 
 //Routes
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'profile', loadComponent: () => import('./components/profile/profile.component').then(m => m.ProfileComponent) },
     { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent) },
     { path: 'register', loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent), canActivate: [userGuard] },
